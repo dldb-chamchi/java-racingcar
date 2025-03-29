cdd6b48aa14014f2cab286693c02a57a6e2cdce3
dldb-chamchi <dladkfl49@gmail.com>
2025-03-29 11:11:07 +0900

    feat: GameController 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,30 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public String inputCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String carName = Console.readLine();
+        if(carName == null || carName.isEmpty()) throw new RuntimeException("자동차 이름을 제대로 입력해주세요.");
+        return carName;
+    }
+
+    public int inputRunCount(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int runCount;
+        try{
+            runCount = Integer.parseInt(Console.readLine());
+            runCountMinusCheck(runCount);
+        }
+        catch (NumberFormatException e) {
+            throw new RuntimeException("정수만 가능합니다.");
+        }
+        return runCount;
+    }
+
+    private void runCountMinusCheck(int runCount){
+        if(runCount <= 0) throw new RuntimeException("실행횟수가 0이하 입니다.");
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,24 @@
 package racingcar.view;
 
+import racingcar.model.RacingCar;
+
+import java.util.List;
+
 public class OutputView {
+
+    public void printGameStart() {
+        System.out.println("\n실행 결과");
+    }
+
+    public void printRound(List<RacingCar> racingCars) {
+        for (RacingCar car : racingCars) {
+            System.out.println(car.getCarName() + " : " + "-".repeat(car.getForwardCount()));
+        }
+        System.out.println();
+    }
+
+    public void printWinner(List<String> winnerCars){
+        System.out.printf("최종 우승자 : %s%n", String.join(", ", winnerCars));
+    }
 }
+
