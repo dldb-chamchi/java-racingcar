f575059d6dfc012b9f3bfad0a368bf169195ff06
dldb-chamchi <dladkfl49@gmail.com>
2025-03-29 10:33:50 +0900

    feat: 패키지 설정

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,25 +1,25 @@
 package racingcar;
 
 public class RacingCar {
-    int forwardCount = 0;
-    String carName;
+    private final String carName;
+    private int forwardCount = 0;
 
-    public RacingCar(String carName) {
-        if(carName.length() > 5) throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+    RacingCar(String carName){
+        if(carName == null || carName.isEmpty()) throw new RuntimeException("제대로된 자동차 이름을 입력해주세요.");
+        if(carName.length() > 5) throw new RuntimeException("자동차 이름은 5자 이하로 설정해주세요.");
         this.carName = carName;
-    }
-
-    public void increaseForwardCount(int randomNumber) {
-        if(randomNumber >= 4) ++forwardCount;
     }
 
     public String getCarName(){
         return carName;
     }
 
+    public void move(int randomNumber){
+        if(randomNumber >= 4) ++forwardCount;
+    }
+
     public int getForwardCount(){
         return forwardCount;
     }
-
 }
 
